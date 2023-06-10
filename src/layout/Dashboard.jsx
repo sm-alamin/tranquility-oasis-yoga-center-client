5bed470c30a40822a2068902bd083c7a4b3001e9
sm-alamin <sma.fkml@gmail.com>
2023-06-10 11:06:05 +0600

    instructor panel is added on dashboard

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,54 +1,122 @@
 import { NavLink, Outlet } from "react-router-dom";
-import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome, FaBook, FaUsers } from 'react-icons/fa';
+import {
+  FaShoppingCart,
+  FaWallet,
+  FaCalendarAlt,
+  FaHome,
+  FaBook,
+  FaUsers,
+} from "react-icons/fa";
 import useCart from "../hooks/useCart";
 import useAdmin from "../hooks/useAdmin";
-
+import useInstructor from "../hooks/useInstructor";
 
 const Dashboard = () => {
-    const [cart] = useCart();
-    const [isAdmin] = useAdmin();
-    return (
-        <div className="drawer lg:drawer-open">
-  <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-  <div className="drawer-content flex flex-col items-center justify-center">
-    {/* Page content here */}
-    <Outlet />
-    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-  
-  </div> 
-  <div className="drawer-side bg-[#D1A054]">
-                <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80">
-
-                    {/* changeable menu according to the role */}
-                    {
-                        isAdmin ? <>
-                            <li><NavLink to="/dashboard/home"><FaHome></FaHome> Admin Home</NavLink></li>
-                            <li><NavLink to="/dashboard/manage-classes"><FaWallet></FaWallet> Manage Classes</NavLink></li>
-                            <li><NavLink to="/dashboard/manage-booking"><FaBook></FaBook> Manage Bookings</NavLink></li>
-                            <li><NavLink to="/dashboard/all-users"><FaUsers></FaUsers> Manage Users</NavLink></li>
-                            
-                        </> : <>
-                            <li><NavLink to="/dashboard/home"><FaHome></FaHome> User Home</NavLink></li>
-                            <li><NavLink to="/dashboard/reservations"><FaCalendarAlt></FaCalendarAlt> Reservations</NavLink></li>
-                            <li><NavLink to="/dashboard/history"><FaWallet></FaWallet> Payment History</NavLink></li>
-                            <li>
-                                <NavLink to="/dashboard/my-cart"><FaShoppingCart></FaShoppingCart> My Cart
-                                    <span className="badge inl badge-secondary">+{cart?.length || 0}</span>
-                                </NavLink>
-
-                            </li>
-                        </>
-                    }
-                    {/* common for all role */}
-                    <div className="divider"></div>
-                    <li><NavLink to="/"><FaHome></FaHome> Home</NavLink> </li>
-                    <li><NavLink to="/classes"> Our Classes</NavLink></li>
-                </ul>
-
-            </div>
-</div>
-    );
+  const [cart] = useCart();
+  const [isAdmin] = useAdmin();
+  const [isInstructor] = useInstructor();
+  return (
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center justify-center">
+        {/* Page content here */}
+        <Outlet />
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-primary drawer-button lg:hidden"
+        >
+          Open drawer
+        </label>
+      </div>
+      <div className="drawer-side bg-[#D1A054]">
+        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80">
+          {/* changeable menu according to the role */}
+          {isAdmin ? (
+            <>
+              <li>
+                <NavLink to="/dashboard/home">
+                  <FaHome></FaHome> Admin Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/manage-classes">
+                  <FaWallet></FaWallet> Manage Classes
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/manage-booking">
+                  <FaBook></FaBook> Manage Bookings
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/all-users">
+                  <FaUsers></FaUsers> Manage Users
+                </NavLink>
+              </li>
+            </>
+          ) : isInstructor ? (
+            <>
+              <>
+                <li>
+                  <NavLink to="/dashboard/home">
+                    <FaHome /> Instructor Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/manage-courses">
+                    <FaBook /> Add a Class
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/manage-schedule">
+                    <FaCalendarAlt /> Manage Classes
+                  </NavLink>
+                </li>
+                {/* Add more instructor-specific menu items */}
+              </>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/dashboard/home">
+                  <FaHome></FaHome> User Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/reservations">
+                  <FaCalendarAlt></FaCalendarAlt> Reservations
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/history">
+                  <FaWallet></FaWallet> Payment History
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/my-cart">
+                  <FaShoppingCart></FaShoppingCart> My Cart
+                  <span className="badge inl badge-secondary">
+                    +{cart?.length || 0}
+                  </span>
+                </NavLink>
+              </li>
+            </>
+          )}
+          {/* common for all role */}
+          <div className="divider"></div>
+          <li>
+            <NavLink to="/">
+              <FaHome></FaHome> Home
+            </NavLink>{" "}
+          </li>
+          <li>
+            <NavLink to="/classes"> Our Classes</NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
